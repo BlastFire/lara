f344bf3b15624491527141a3d6a83356fc1af4ba
defaxed <defaxed@gmail.com>
2015-11-24 17:59:47 +0200

    auth routes and ui, migration changes in user table

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,11 +15,20 @@
 //     return view('welcome');
 // });
 
-Route::get('/', 'WelcomeController@contact');
+// Authentication routes...
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+// Registration routes...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
+
 
 // Route::get('articles', 'ArticlesController@index');
 // Route::get('articles/create', 'ArticlesController@create');
 // Route::get('articles/{id}', 'ArticlesController@show');
 // Route::post('articles', 'ArticlesController@store');
 
+Route::get('/', 'WelcomeController@contact');
 Route::resource('articles', 'ArticlesController');
