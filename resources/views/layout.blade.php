1405edfa68fa504d886ef9856bfa28d65166de6d
vagrant <vagrant@homestead>
2015-12-06 17:53:20 +0000

    clean up

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,18 +1,21 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en-US">
 <head>
-	<meta charset="UTF-8">
-	<title>Document</title>
+	<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+	<title>Put title here</title>
 	<link rel="stylesheet" type="text/css" href="css/app.css">
 	<link rel="stylesheet" type="text/css" href="css/all.css">
+	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Wellfleet">
 </head>
 <body>
 	<header>
 		@include('grizmin.header')
 	</header>
 
-	<div class="container">
-		@yield('content')
+	<div id="w">
+		<div class="container">
+			@yield('content')
+		</div>
 	</div>
 
 </body>
